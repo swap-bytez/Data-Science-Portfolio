f39287ce0fa1489e4f8d88a8df46b3daf11d8643
swap-bytez <swapnilprakashmhatre@gmail.com>
2026-09-14 20:14:33 -0700

    Update hello.py
    Create greeting project

--- projects/greeting-project/hello.py
+++ projects/greeting-project/hello.py
@@ -1,1 +1,7 @@
+# My first Python project in GitHub
+def greet_user():
+name = input("Enter your name: ")
+print(f"Hello, {name}! Welcome to Data Science.")
 
+if __name__ == "__main__":
+greet_user()
